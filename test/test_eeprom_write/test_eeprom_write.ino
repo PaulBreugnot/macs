cd1afd55585d0553dd7a6625d5a67c30b5f02766
PaulBreugnot <paul.breugnot@etu.emse.fr>
2018-03-29 12:27:35 +0200

    Creating test folder with test_eeprom_write.ino

---
+++ test/test_eeprom_write/test_eeprom_write.ino
@@ -0,0 +1,75 @@
+/*
+ * EEPROM Write
+ *
+ * Stores values read from analog input 0 into the EEPROM.
+ * These values will stay in the EEPROM when the board is
+ * turned off and may be retrieved later by another sketch.
+ */
+
+#include <EEPROM.h>
+
+#define KEY_NUM_EEPROM_HIGH     KEY_NUM_EEPROM_LOW-1
+#define KEY_NUM_EEPROM_LOW      KEY_CHECK_EEPROM_HIGH-1
+#define KEY_CHECK_EEPROM_HIGH   KEY_CHECK_EEPROM_LOW-1
+#define KEY_CHECK_EEPROM_LOW    EEPROM_MAX
+#define EEPROM_MAX 2048
+
+/** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
+  uint16_t num_keys = 0;
+  uint16_t num_keys_check = 0;
+int addr;
+uint32_t tagToWrite = 0;
+
+void setup() {
+  Serial.begin(9600);
+  EEPROM.begin(EEPROM_MAX);
+  set_addr();
+  write_new_tag();
+  update_numKeys();
+  EEPROM.commit();
+}
+
+void loop() {
+}
+
+void set_addr(){
+  uint8_t temp;
+
+  temp = EEPROM.read(KEY_NUM_EEPROM_HIGH);
+  num_keys = temp << 8;
+  temp = EEPROM.read(KEY_NUM_EEPROM_LOW);
+  num_keys += temp;
+
+  Serial.print("# of keys =");
+  Serial.println(num_keys);
+
+  temp = EEPROM.read(KEY_CHECK_EEPROM_HIGH);
+  num_keys_check = temp << 8;
+  temp = EEPROM.read(KEY_CHECK_EEPROM_LOW);
+  num_keys_check += temp;
+
+  addr = num_keys*4;
+}
+
+void write_new_tag(){
+  EEPROM.write(addr, (tagToWrite>>24)&0xFF);
+  EEPROM.write(addr, (tagToWrite>>16)&0xFF);
+  EEPROM.write(addr, (tagToWrite>>8)&0xFF);
+  EEPROM.write(addr, (tagToWrite)&0xFF);
+  num_keys+=1;
+}
+
+void update_numKeys(){
+  Serial.print("write:");
+  Serial.println(KEY_NUM_EEPROM_LOW);
+
+  EEPROM.write(KEY_NUM_EEPROM_HIGH, (num_keys >> 8) & 0xff);
+  EEPROM.write(KEY_NUM_EEPROM_LOW, (num_keys) & 0xff);
+  // checksum
+  Serial.print("write:");
+  Serial.println(KEY_CHECK_EEPROM_LOW);
+  EEPROM.write(KEY_CHECK_EEPROM_HIGH, ((num_keys + 1) >> 8) & 0xff);
+  EEPROM.write(KEY_CHECK_EEPROM_LOW, ((num_keys + 1)) & 0xff);
+  EEPROM.commit();
+}
+
